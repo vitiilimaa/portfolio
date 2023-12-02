4898b186fa9a6d5b061442d8f9715840df189f51
Vitor Batista <vitiilimaa@gmail.com>
2023-12-02 16:59:17 -0300

    Ajuste de bug ao maximizar um projeto

--- src/sections/Projects/index.tsx
+++ src/sections/Projects/index.tsx
@@ -1,27 +1,15 @@
 import styles from "./styles.module.css";
 import { TitleSection, SystemXPWindow } from "../../components";
 import systemXPWindowData from "../../data/systemXPWindowData";
-import { useInView, animated } from "@react-spring/web";
 
 const Projects = () => {
-  const [ref, springs] = useInView(() => ({
-    from: {
-      transform: "scale(0)",
-    },
-    to: {
-      transform: "scale(1)",
-    },
-  }));
-
   return (
     <section
       id="projects"
       className={`${styles.desktop} row text-center gap-5 pb-5 gap-xl-0`}
     >
       <TitleSection addClassToContainer="mt-5" caption="Projetos" />
-      <animated.div
-        ref={ref}
-        style={springs}
+      <div
         className="col-12 d-flex flex-wrap justify-content-around align-items-around gap-5 mt-xl-5"
       >
         {systemXPWindowData.map((data, key) => (
@@ -33,7 +21,7 @@ const Projects = () => {
             urlProject={data.urlProject}
           />
         ))}
-      </animated.div>
+      </div>
     </section>
   );
 };
